1bd5739f5f0714a2dc823a45f0c597932ad11987
Abhishek4298 <abhishekbhavsar143@gmail.com>
2024-06-12 08:43:41 +0530

    Basic props pass

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import MyButton from "./components/MyButton";
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton title="I'm a button" />
+    </div>
+  );
+}
---
+++ src/components/MyButton.tsx
@@ -0,0 +1,9 @@
+const MyButton = ({ title }: { title: string }) => {
+  return (
+    <>
+      <button>{title}</button>
+    </>
+  );
+};
+
+export default MyButton;
